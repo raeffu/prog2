bcd406495bd1b9078d9631ca75839d43b1e28417
raeffu <raeffu@raeffu.ch>
2014-02-18 10:39:20 +0000

    add excercises inheritance

---
+++ src/_excercises/ch09/Employee.java
@@ -0,0 +1,24 @@
+package _excercises.ch09;
+
+public class Employee extends Person {
+  private String _department;
+
+  public Employee(String name, String email, String department){
+    super(name, email);
+    _department = department;
+  }
+  
+  public String get_department() {
+    return _department;
+  }
+
+  public void set_department(String department) {
+    _department = department;
+  }
+
+  @Override
+  public String getDescription() {
+    return "Employee works in " + _department;
+  }
+  
+}
