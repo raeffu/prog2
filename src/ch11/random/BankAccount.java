b373a7ceffe162eb6f3918175b372bf2332fa184
raeffu <raeffu@raeffu.ch>
2014-03-05 22:05:08 +0000

    add initail random file access task

--- src/ch11/random/BankAccount.java
+++ src/ch11/random/BankAccount.java
@@ -1,70 +1,87 @@
 package ch11.random;
 
 /**
-   A bank account has a balance that can be changed by 
-   deposits and withdrawals.
-*/
-public class BankAccount
-{  
-   private int accountNumber;
-   private double balance;
+ * A bank account has a balance that can be changed by deposits and withdrawals.
+ */
+public class BankAccount {
+  private int accountNumber;
+  private double balance;
+  private float interestRate;
+  private String owner;
 
-   /**
-      Constructs a bank account with a zero balance.
-      @param anAccountNumber the account number for this account
+  /**
+   * Constructs a bank account with a zero balance.
+   * 
+   * @param anAccountNumber
+   *          the account number for this account
    */
-   public BankAccount(int anAccountNumber)
-   {   
-      accountNumber = anAccountNumber;
-      balance = 0;
-   }
+  public BankAccount(int anAccountNumber) {
+    accountNumber = anAccountNumber;
+    balance = 0;
+    interestRate = 0.02f;
+  }
 
-   /**
-      Constructs a bank account with a given balance.
-      @param anAccountNumber the account number for this account
-      @param initialBalance the initial balance
+  /**
+   * Constructs a bank account with a given balance.
+   * 
+   * @param anAccountNumber
+   *          the account number for this account
+   * @param initialBalance
+   *          the initial balance
    */
-   public BankAccount(int anAccountNumber, double initialBalance)
-   {   
-      accountNumber = anAccountNumber;
-      balance = initialBalance;
-   }
+  public BankAccount(int anAccountNumber, double initialBalance,
+      float interestRate, String owner) {
+    accountNumber = anAccountNumber;
+    balance = initialBalance;
+    this.interestRate = interestRate;
+    this.owner = owner;
+  }
 
-   /**
-      Gets the account number of this bank account.
-      @return the account number
+  /**
+   * Gets the account number of this bank account.
+   * 
+   * @return the account number
    */
-   public int getAccountNumber()
-   {   
-      return accountNumber;
-   }
+  public int getAccountNumber() {
+    return accountNumber;
+  }
 
-   /**
-      Deposits money into the bank account.
-      @param amount the amount to deposit
+  /**
+   * Deposits money into the bank account.
+   * 
+   * @param amount
+   *          the amount to deposit
    */
-   public void deposit(double amount)
-   {  
-      double newBalance = balance + amount;
-      balance = newBalance;
-   }
+  public void deposit(double amount) {
+    double newBalance = balance + amount;
+    balance = newBalance;
+  }
 
-   /**
-      Withdraws money from the bank account.
-      @param amount the amount to withdraw
+  /**
+   * Withdraws money from the bank account.
+   * 
+   * @param amount
+   *          the amount to withdraw
    */
-   public void withdraw(double amount)
-   {   
-      double newBalance = balance - amount;
-      balance = newBalance;
-   }
+  public void withdraw(double amount) {
+    double newBalance = balance - amount;
+    balance = newBalance;
+  }
 
-   /**
-      Gets the current balance of the bank account.
-      @return the current balance
+  /**
+   * Gets the current balance of the bank account.
+   * 
+   * @return the current balance
    */
-   public double getBalance()
-   {   
-      return balance;
-   }
+  public double getBalance() {
+    return balance;
+  }
+
+  public float getInterest() {
+    return this.interestRate;
+  }
+
+  public String getOwner() {
+    return owner;
+  }
 }
--- src/ch11/random/BankSimulator.java
+++ src/ch11/random/BankSimulator.java
@@ -4,64 +4,65 @@ import java.io.IOException;
 import java.util.Scanner;
 
 /**
-   This program demonstrates random access. You can access existing
-   accounts and deposit money, or create new accounts. The
-   accounts are saved in a random access file.
-*/
-public class BankSimulator
-{  
-   public static void main(String[] args) throws IOException
-   {  
-      Scanner in = new Scanner(System.in);
-      BankData data = new BankData();
-      try
-      {  
-         data.open("bank.dat");
+ * This program demonstrates random access. You can access existing accounts and
+ * deposit money, or create new accounts. The accounts are saved in a random
+ * access file.
+ */
+public class BankSimulator {
 
-         boolean done = false;
-         while (!done)
-         {  
-            System.out.print("Account number: ");
-            int accountNumber = in.nextInt();
-            System.out.print("Amount to deposit: ");
-            double amount = in.nextDouble();
+  public static void main(String[] args) throws IOException {
+    Scanner in = new Scanner(System.in);
+    BankData data = new BankData();
 
-            int position = data.find(accountNumber);
-            BankAccount account;
-            if (position >= 0)
-            {
-               account = data.read(position);
-               account.deposit(amount);
-               System.out.println("New balance: " + account.getBalance());
-            }
-            else // Add account
-            {  
-               account = new BankAccount(accountNumber, amount);
-               position = data.size();
-               System.out.println("Adding new account.");
-            }
-            data.write(position, account);
+    try {
+      data.open("bank.dat");
 
-            System.out.print("Done? (Y/N) ");
-            String input = in.next();
-            if (input.equalsIgnoreCase("Y")) done = true;
-         }      
+      boolean done = false;
+
+      while (!done) {
+        System.out.print("Account number: ");
+        int accountNumber = in.nextInt();
+
+        if (accountNumber <= 0) {
+//          deleteAccount();
+        }
+        else {
+
+          System.out.print("Amount to deposit: ");
+          double amount = in.nextDouble();
+
+          int position = data.find(accountNumber);
+          BankAccount account;
+
+          if (position >= 0) {
+            account = data.read(position);
+            account.deposit(amount);
+            System.out.println("New balance: " + account.getBalance());
+            System.out.println("interest rate: " + account.getInterest());
+            System.out.println("Owner: " + account.getOwner());
+          }
+          else // Add account
+          {
+
+            System.out.print("Interest rate: ");
+            float interest = in.nextFloat();
+            System.out.print("Owner: ");
+            String owner = in.next();
+
+            account = new BankAccount(accountNumber, amount, interest, owner);
+            position = data.size();
+            System.out.println("Adding new account.");
+          }
+
+          data.write(position, account);
+
+          System.out.print("Done? (Y/N) ");
+          String input = in.next();
+          if (input.equalsIgnoreCase("Y")) done = true;
+        }
       }
-      finally
-      {
-         data.close();
-      }
-   }
+    } finally {
+      data.close();
+    }
+  }
 }
-
-
-
-
-
-
-
-
-
-
-
-
