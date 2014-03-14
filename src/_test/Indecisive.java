bc3f0f40b5be9b50a5b38f445f199634e0b22875
raeffu <raeffu@raeffu.ch>
2014-03-14 17:04:48 +0100

    add new tasks

---
+++ src/_test/Indecisive.java
@@ -0,0 +1,16 @@
+package _test;
+
+public class Indecisive {
+  
+  public static void main(String[] args) {
+    System.out.println(decision());
+  }
+
+  private static boolean decision() {
+    try {
+      return true;
+    } finally {
+      return false;
+    }
+  }
+}
--- src/ch11/exception/listOfNumbers/ListOfNumbers.java
+++ src/ch11/exception/listOfNumbers/ListOfNumbers.java
@@ -1,40 +1,52 @@
 package ch11.exception.listOfNumbers;
 
 import java.io.*;
+import java.util.Scanner;
 import java.util.Vector;
 
 public class ListOfNumbers {
-	private Vector<Integer> victor;
-	private static final int SIZE = 10;
+  private Vector<Integer> victor;
+  private static final int SIZE = 10;
 
-	public ListOfNumbers() {
-		victor = new Vector<Integer>(SIZE);
-		for (int i = 0; i < SIZE; i++)
-			victor.addElement(new Integer(i));
-	}
+  public ListOfNumbers() {
+    victor = new Vector<Integer>(SIZE);
+    for (int i = 0; i < SIZE; i++)
+      victor.addElement(new Integer(i));
+  }
 
-	public void writeList() {
-		PrintWriter out = null;
+  public void writeList() {
 
-		try {
-			out = new PrintWriter("OutFile.txt");
-			try {
-				for (int i = 0; i < victor.size(); i++)
-					out.println("Value at: " + i + " = " + victor.elementAt(i));
-			} finally {
-				System.out.println("Closing PrintWriter");
-				out.close();
-			}
+    try (PrintWriter out = new PrintWriter("OutFile.txt")) {
+      for (int i = 0; i < victor.size(); i++)
+        out.println("Value at: " + i + " = " + victor.elementAt(i));
 
-		} catch (ArrayIndexOutOfBoundsException e) {
-			System.err.println("Caught ArrayIndexOutOfBoundsException: "
-					+ e.getMessage());
-		} catch (FileNotFoundException e) {
-			System.err.println("Caught FileNotFoundException: "
-					+ e.getMessage());
-		} catch (SecurityException e) {
-			System.err.println("Caught FileNotFoundException: "
-					+ e.getMessage());
-		}
-	}
+      System.out.println("Closing PrintWriter");
+
+    }
+    catch (ArrayIndexOutOfBoundsException e) {
+      System.err.println("Caught ArrayIndexOutOfBoundsException: "
+          + e.getMessage());
+    }
+    catch (FileNotFoundException e) {
+      System.err.println("Caught FileNotFoundException: " + e.getMessage());
+    }
+  }
+  
+  public void readList(){
+    FileReader reader;
+    try {
+      reader = new FileReader("InFile.txt");
+      Scanner s = new Scanner(reader);
+      
+      while (s.hasNextInt()) {
+        int i = s.nextInt();
+        System.out.println(i);
+      }
+    }
+    catch (FileNotFoundException e) {
+      // TODO Auto-generated catch block
+      e.printStackTrace();
+    }
+    
+  }
 }
